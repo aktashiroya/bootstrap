13f685d9364f00ac49f7e319d755ee0e991a78d6
aktashiroya <akta8758@gmail.com>
2022-12-01 11:40:23 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="row justify-content-between">
+        <div className='col logo'><a href="#"><img src={require("./img/logo.png")} alt="loding..."/></a></div>
+        <div className='col-auto'>
+          <ul className='d-flex main_menu align-items-center'>
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#">MINISTRY</a></li>
+              <li><a href="#">SERMONS</a></li>
+              <li><a href="#">PAGES</a></li>
+              <li><a href="#">CONTACT</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
-
 export default App;
